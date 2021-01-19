d805249f32e80edae2b866b0fc080adc34249b05
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2021-01-19 01:10:09 -0600

    mapping the program, some functions still need to be written

--- src/main.c
+++ src/main.c
@@ -43,9 +43,62 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <getopt.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <pthread.h>
 
 #include "log.h"
 
+struct addrinfo
+{
+  int ai_flags;	            /* Input flags.  */
+  int ai_family;            /* Protocol family for socket.  */
+  int ai_socktype;          /* Socket type.  */
+  int ai_protocol;          /* Protocol for socket.  */
+  socklen_t ai_addrlen;     /* Length of socket address.  */
+  struct sockaddr *ai_addr; /* Socket address for socket.  */
+  char *ai_canonname;       /* Canonical name for service location.  */
+  struct addrinfo *ai_next; /* Pointer to next in list.  */
+};
+
+int   server_index;
+char* server_buffer;
+char* msg;
+
+#define BUFF_SIZE 2048
+#define TRUE  1
+#define FALSE 0
+#define AI_PASSIVE 1
+#define NICK_FLAG 1
+#define USER_FLAG 1 << 1
+
+int assemble_and_check(char* recv_buf, char* server_buf, int* i) {
+    /* put the thing from recv_buf into server_buf */
+    /* update index */
+    /* see if end of recv_buf has carriage return */
+    /* if yes, return 1, otherwise return 0 */
+    return 0;
+}
+
+void extract_full(char* extract_to_buf, char* extract_from_buf, int* i) {
+    /* copy the elements in extract_from_buf until the carriage return to 
+       extract_to_buf */
+    /* shift all the elements of extact_from_buf down */
+    /* update i */
+}
+
+int strcmp_partial(char* str1, char* str2, int until) {
+    if (!str1 || !str2)
+        return -1;
+    for (int i = 0; i < until; i++)
+        if (str1[i] != str2[i])
+            return 0;
+    return 1;
+}
 
 int main(int argc, char *argv[])
 {
@@ -122,6 +175,114 @@ int main(int argc, char *argv[])
 
     /* Your code goes here */
 
+    int clientsoc, serversoc;
+    struct addrinfo *serveraddr, *p, hints;
+    struct sockaddr_storage *clientaddr;
+
+    server_index  = 0;
+    server_buffer = calloc(BUFF_SIZE, sizeof(char));
+    int yes = 1;
+    socklen_t sin_size = sizeof(struct sockaddr_storage);
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+    hints.ai_flags = AI_PASSIVE; /* For wildcard IP address */
+
+    /* get server socket with getaddrinfo */
+    int s = getinfoaddr(NULL, port, &hints, &serveraddr);
+    if (s != 0) {
+        perror("getaddrinfo() failed!");
+        exit(1);
+    }
+
+    for (p = serveraddr; p != NULL; p = p->ai_next) {
+        if ((serversoc = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("Could not open socket");
+            continue;
+        }
+
+        if (setsockopt(serversoc, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+            perror("Socket setsockopt() failed");
+            close(serversoc);
+            continue;
+        }
+
+        if (bind(serversoc, p->ai_addr, p->ai_addrlen) == -1){
+            perror("Socket bind() failed");
+            close(serversoc);
+            continue;
+        }
+
+        if (listen(serversoc, 5) == -1) {
+            perror("Socket listen() failed");
+            close(serversoc);
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(serveraddr);
+
+    if (p == NULL) {
+        fprintf(stderr, "Could not find a socket to bind to.\n");
+        exit(1);
+    }
+
+    char buf[512];
+    char current_nick[512];
+    char current_user[512];
+    memset(buf, 0, 512);
+    memset(current_nick, 0, 512);
+    memset(current_user, 0, 512);
+
+    int nick_and_user_flags = 0;
+
+    /* Wait to accept client connections */
+    while(TRUE) {
+        clientaddr = calloc(1, sin_size);
+        s = clientsoc = accept(serversoc, (struct sockaddr*) clientaddr, &sin_size);
+        if (s == -1) {
+            free(clientaddr);
+            perror("Could not accept() connection");
+            /* connect to client loop */
+            continue;
+        }
+
+        /* currently connected to client */
+        recv(clientsoc, buf, 512, 0);
+        if (assemble_and_check(buf, server_buffer, &server_index)) {
+            char* assembled = calloc(512, sizeof(char));
+            extract_full(assembled, server_buffer, &server_index);
+
+            /* if we got a user command or a nick command, set that flag */
+            if (strcmp_partial(assembled, "USER", 4)) {
+                nick_and_user_flags |= USER_FLAG;
+                /* parse into current_user */
+            } else if (strcmp_partial(assembled, "NICK", 4)) {
+                nick_and_user_flags |= NICK_FLAG;
+                /* parse into current_nick */
+            }
+
+            /* if we have both a nick and user input, send the welcome message */
+            if (nick_and_user_flags & NICK_FLAG & USER_FLAG) {
+                sprintf(msg, ":bar.example.com 001 %s :Welcome to the Internet Relay Network user1!user1@foo.example.com\r\n",
+                    current_nick);
+                send(clientsoc, msg, strlen(msg), 0);
+
+                /* clear flags */
+                nick_and_user_flags = 0;
+                /* clear current nick and user */
+                memset(current_nick, 0, 512);
+                memset(current_user, 0, 512);
+            }
+            free(assembled);
+        }
+    }
+
+    close(serversoc);
+    free(server_buffer);
     return 0;
 }
 
